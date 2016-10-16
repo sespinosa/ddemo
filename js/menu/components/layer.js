099a6591d84106e9c50bf60e973bb44f65a63b3a
Sebastian Espinosa <sespinosar@gmail.com>
2016-10-16 11:08:31 +0000

    Added configuration file to add buttons with text (very limited text)

--- js/menu/components/layer.js
+++ js/menu/components/layer.js
@@ -1,26 +1,15 @@
 import cfgLayer from './layer.json'
+import cfgButtonsText from './buttons_text.json'
 import { roundRectangle } from '../../utils/shapes'
 import { renderBtnText } from './button'
 
+
+const renderButtonsText = () => {
+  cfgButtonsText.forEach((btn) => renderBtnText(...btn))
+}
+
+
 export default () => {
-
-  renderBtnText(45, 100, 50, 50, 'S')
-  renderBtnText(105, 100, 50, 50, 'T')
-  renderBtnText(165, 100, 50, 50, 'D')
-
-  renderBtnText(45, 160, 50, 50, 'H')
-  renderBtnText(105, 160, 50, 50, 'D')
-  renderBtnText(165, 160, 50, 50, 'P')
-
-  renderBtnText(45, 220, 50, 50, 'C')
-  renderBtnText(105, 220, 50, 50, 'T')
-  renderBtnText(165, 220, 50, 50, 'M')
-
-  renderBtnText(45, 280, 110, 50, 'HDP')
-
-  renderBtnText(45, 340, 170, 50, 'RECTM')
-
-  renderBtnText(45, 400, 170, 170, 'SOSICK')
-
+  renderButtonsText()
   roundRectangle(cfgLayer)
 }
